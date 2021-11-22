3f5ed8c61809e8792308dd062af252a9c0270699
DimAshQZ <53437421+DimAshQZ@users.noreply.github.com>
2021-11-22 09:43:11 +0600

    2.3. Введение в Maven и Spring Boot HW

--- src/main/java/pro/sky/calculator/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/calculator/CalculatorServiceImpl.java
@@ -13,6 +13,7 @@ public class CalculatorServiceImpl implements CalculatorService {
         return num1 + num2;
     }
 
+
     @Override
     public int calcMinus(int num1, int num2) {
         return num1 - num2;
