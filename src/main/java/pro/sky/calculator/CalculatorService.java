e8c8e589f3d4298097b4b730cefd1ff652e3cfe1
DimAshQZ <53437421+DimAshQZ@users.noreply.github.com>
2021-11-22 09:37:46 +0600

    init

---
+++ src/main/java/pro/sky/calculator/CalculatorService.java
@@ -0,0 +1,13 @@
+package pro.sky.calculator;
+
+public interface CalculatorService {
+    String toGreet();
+
+    int calcPlus(int num1, int num2);
+
+    int calcMinus(int num1, int num2);
+
+    int calcMultiply(int num1, int num2);
+
+    int calcDivide(int num1, int num2);
+}
---
+++ src/main/java/pro/sky/calculator/CalculatorServiceImpl.java
@@ -0,0 +1,35 @@
+package pro.sky.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+    public String toGreet() {
+        return "Добро пожаловать в калькулятор.";
+    }
+
+    @Override
+    public int calcPlus(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    @Override
+    public int calcMinus(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    @Override
+    public int calcMultiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    @Override
+    public int calcDivide(int num1, int num2) {
+        if (num2 == 0) {
+            return 0;
+        } else {
+            return num1 / num2;
+        }
+    }
+
+}
